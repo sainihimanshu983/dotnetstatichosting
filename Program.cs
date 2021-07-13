f2c746e6ce6c2eb2b9678f565d79d3f826617548
Himanshu <himanshu05@nagarro.com>
2021-07-13 11:18:52 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetCoreSqlDb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddAzureWebAppDiagnostics();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Dot Net Static Hosting
+
+This repository is a simple codebase of dotnet to host static file content. This static file content can be an output from angular, reactJS, vanilla HTML or media. Default redirect is not enabled, it can be enabled if use-case suggests. 
